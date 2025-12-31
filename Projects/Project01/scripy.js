41dadd8f4728169251167b93bf60360863519e1b
Vaex Mungaji <vaexmungajijr@gmail.com>
2025-12-31 11:03:34 +0100

    Adicionei um menú hamburguer

---
+++ Projects/Project01/scripy.js
@@ -0,0 +1,6 @@
+let btnmenu = document.getElementById("btnmenu");
+let menu = document.getElementById("smenu");
+
+btnmenu.addeventListener("click", function(){
+    document.body.style.backgroundColor = "lightblue";
+}
